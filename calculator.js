517970a96bcf04c78b6960007f30b2c2c1f2b9a3
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2023-10-03 22:35:20 +0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let displayValue = '';
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.value = displayValue;
+}
+
+function appendToDisplay(val) {
+    displayValue += val;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
